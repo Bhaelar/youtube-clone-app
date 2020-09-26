77f0318ed8988d97dc9dc2e78e4c7719dbcf85ca
Bhaelar <okenlabasit@gmail.com>
2020-09-26 15:42:50 +0100

    Updates

---
+++ precache-manifest.cac4bb05d972d09b12109539f5469a6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c46bb5464e0ad6afc31fefac5e0e64e",
+    "url": "/youtube-clone-app/index.html"
+  },
+  {
+    "revision": "5e818e4aab817b1921f4",
+    "url": "/youtube-clone-app/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "9d4b0aca24a5303ef4c9",
+    "url": "/youtube-clone-app/static/js/2.d108ce99.chunk.js"
+  },
+  {
+    "revision": "5e818e4aab817b1921f4",
+    "url": "/youtube-clone-app/static/js/main.9dec29d3.chunk.js"
+  },
+  {
+    "revision": "5a5091a5dc1aaa85903f",
+    "url": "/youtube-clone-app/static/js/runtime-main.441f28f1.js"
+  }
+]);
